40a4a0cc3bf9fb84b5dd8f6e73beead7aa9bf424
FoxCzz <ondrej.velechovsky@student.spsmb.cz>
2025-05-23 10:54:16 +0200

    tailwind

--- Wafles/server/app.js
+++ Wafles/server/app.js
@@ -29,7 +29,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use("/people", peopleRouter);
 app.use("/wafles", waflesRouter);
 
 // catch 404 and forward to error handler
